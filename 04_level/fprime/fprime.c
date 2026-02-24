41da3975507f05958bfbcd2aab172e4b4641f38d
Michael Christopher Gubatan Velasquez <mvelasqu@c2r6s13.42singapore.sg>
2026-02-24 12:46:53 +0800

    fprime re do

--- 04_level/fprime/fprime.c
+++ 04_level/fprime/fprime.c
@@ -4,36 +4,39 @@
 
 int main(int argc, char **argv)
 {
-    int n;
-    int i;
-    int first = 1;
+	int i;
+	int n;
+	int first;
 
-    if (argc == 2)
-    {
-        n = atoi(argv[1]);
-        if (n == 1)
-            printf("1");
-        else
-        {
-            i = 2;
-            while (i * i <= n)
-            {
-                while (n % i == 0)
-                {
-                    if (!first) printf("*");
-                    printf("%d", i);
-                    first = 0;
-                    n /= i;
-                }
-                i++;
-            }
-            if (n > 1)
-            {
-                if (!first) printf("*");
-                printf("%d", n);
-            }
-        }
-    }
-    printf("\n");
-    return 0;
+	if (argc == 2)
+	{
+		first = 0;
+		n = atoi(argv[1]);
+		if(n == 1)
+			printf("1");
+		else
+		{
+			i = 2;
+			while(i * i <= n)
+			{
+				while (n % i == 0)
+				{
+					if(first)
+						printf("*");
+					printf("%d",i);
+					first = 1;
+					n = n / i;
+				}
+				i++;
+			}
+			if (n > 1)
+			{
+				if(first)
+					printf("*");
+				printf("%d",n);
+			}
+		}
+	}
+	printf("\n");
+	return 0;
 }
