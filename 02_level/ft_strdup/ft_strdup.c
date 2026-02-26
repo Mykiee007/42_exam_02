2ad3ef2b19d2e37a0e3020994682d88803350cf0
Mykiee007 <velasquez.michaelchristopher@gmail.com>
2026-02-26 08:35:53 +0800

    check

---
+++ 02_level/ft_strdup/ft_strdup.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+char    *ft_strdup(char *src) {
+    char    *dup;
+    int     i = 0;
+
+    while(src[i] != '\0')
+        i++;
+    dup = (char *)malloc((i + 1) * sizeof(char));
+    if (!dup)
+        return NULL;
+    i = 0;
+    while(src[i] != '\0') {
+        dup[i] = src[i];
+        i++;
+    }
+    dup[i] = '\0';
+    return (dup);
+}
+#include <stdio.h>
+int main(void){
+    char    *s = "hello world";
+    char    *d = ft_strdup(s);
+    printf("%s\n", d);
+    free(d);
+    return 0;
+}
