bab2e39f3498fa0f25b4636d4ab6a92e96a385e8
Michael Christopher Gubatan Velasquez <mvelasqu@c2r6s13.42singapore.sg>
2026-03-06 11:56:21 +0800

    test 6 done

--- 04_level/sort_in_tab/sort_int_tab.c
+++ 04_level/sort_in_tab/sort_int_tab.c
@@ -1,4 +1,70 @@
+//11:23 am 11:55am
+
 #include <stdio.h>
+
+
+static int is_ascend(int *tab, unsigned int size)
+{
+	int i = 0;
+	while(i < (size - 1))
+	{
+		if(tab[i] > tab[i + 1])
+			return 0;
+		i++;
+	}
+	return 1;
+}
+
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	int temp;
+	if(!tab)
+		return;
+	if(size == 0)
+		return;
+	
+	while(!is_ascend(tab, size))
+	{
+		int i = 0;
+		while(i < (size - 1))
+		{
+			if(tab[i] > tab[i + 1])
+			{
+				temp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = temp;
+			}
+			i++;
+		}
+	}
+}
+
+int main(void)
+{
+	int arr[] = { 1, 9 ,4,4, 8,2,11,3,1224,200};
+	int i;
+	unsigned int size = 5;
+
+	printf("Orignial:\n");
+	i = 0;
+	while(arr[i])
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+	printf("\nSorted:\n");
+	sort_int_tab(arr, size);
+	i = 0;
+	while(arr[i])
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+	return 0;
+}
+
+
+/* #include <stdio.h>
 // time 1:40 pm ~ 2:17 pm
 
 
@@ -60,4 +126,4 @@ int main(void)
 		i++;
 	}
 	return 0;
-}
+}*/
