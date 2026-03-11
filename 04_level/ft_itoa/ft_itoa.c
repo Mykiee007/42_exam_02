c6d8e878d05ad93b5b78c5df3e7c97ac8fca489f
Michael Christopher Gubatan Velasquez <mvelasqu@c2r6s13.42singapore.sg>
2026-03-11 14:10:31 +0800

    itoa

--- 04_level/ft_itoa/ft_itoa.c
+++ 04_level/ft_itoa/ft_itoa.c
@@ -1,4 +1,59 @@
 #include <stdlib.h>
+
+char	*ft_itoa(int nbr)
+{
+	long	n;
+	int len;
+	char *s;
+
+	len = 0;
+	n = nbr;
+	while(n)
+	{
+		len++;
+		n/=10;
+	}
+	if(nbr <= 0)
+		len++;
+	s = (char *)malloc((len + 1) * sizeof(char));
+	if(!s)
+		return (NULL);
+	n = nbr;
+	if(n < 0)
+		n = -n;
+	s[len] = '\0';
+	len--;
+	if(n == 0)
+		s[len] = '0';
+	while(n)
+	{
+		s[len] = n % 10 + '0';
+		n /= 10;
+		len--;
+	}
+	if(nbr < 0)
+		s[len] = '-';
+	return (s);
+}
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	char *s;
+	int n;
+
+	if(argc == 2)
+	{
+		n = atoi(argv[1]);
+		s = ft_itoa(n);
+		printf("%s\n", s);
+	}
+	return 0;
+}
+
+
+/*#include <stdlib.h>
 #include <stdio.h>
 
 // need more practice in logic analysis
@@ -39,4 +94,4 @@ int main(void)
 	printf("here is the result: %s\n", ft_itoa(x));
 	free(ft_itoa(x));
 	return 0;
-}
+}*/
